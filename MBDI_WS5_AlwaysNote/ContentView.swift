6ae3cb5554617707f35d1b52b23459a6cb4f02fa
Tim Kaerts <tkaerts@avans.nl>
2022-03-23 12:46:26 +0100

    Initial Commit

---
+++ MBDI_WS5_AlwaysNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MBDI_WS5_AlwaysNote
+//
+//  Created by Tim Kaerts on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MBDI_WS5_AlwaysNote/MBDI_WS5_AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS5_AlwaysNoteApp.swift
+//  MBDI_WS5_AlwaysNote
+//
+//  Created by Tim Kaerts on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS5_AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
